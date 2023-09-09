f5c3035833e9f94c4d246713eec75e53361a83b5
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-09-09 20:51:49 +0200

    Set sampleData as a State

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 import CVDisplay from './components/CVDisplay';
 import { sampleData } from './sampleData';
 
-/**/
-
 function App() {
+  const [userData, setUserData] = useState(sampleData);
   return (
     <div className="w-full min-h-screen p-3">
-      <CVDisplay data={sampleData} />
+      <CVDisplay data={userData} />
     </div>
   );
 }
