e2024dcd57e33d66135bf36c6582c0ef16308a7a
Isidro Navarro Oporto <99091821+IsiNavarro@users.noreply.github.com>
2023-09-11 19:45:51 +0200

    Display current experiences on FillIn

--- src/components/FillIn.jsx
+++ src/components/FillIn.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaUserCircle, FaUniversity } from 'react-icons/fa';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
 import { GiSuitcase } from 'react-icons/gi';
 
 function FillInPersonalInfo({ data, handlePersonalInfo }) {
@@ -77,17 +78,36 @@ function FillInEducation({ data }) {
 }
 
 function FillInExperience({ data }) {
+  const ExperienceItem = ({ title }) => {
+    return (
+      <div className="flex items-center justify-between border-t-4 py-2">
+        <h4 className="text-lg">{title}</h4>
+        <button className="p-1 hover:text-slate-800 duration-50">
+          <RiDeleteBin5Fill size={22} />
+        </button>
+      </div>
+    );
+  };
+  const ExperienceList = ({ experience }) => {
+    console.log(experience);
+    return experience.map((item) => (
+      <ExperienceItem key={item.id} title={item.companyName} />
+    ));
+  };
   return (
     <div className="w-full py-8 px-6 bg-white rounded-md">
       <h3 className="font-bold text-2xl flex gap-2">
         <GiSuitcase size={30} />
         Experience
       </h3>
+      <div className="flex flex-col gap-2 p-4">
+        <ExperienceList experience={data.sections.experiences} />
+      </div>
     </div>
   );
 }
 
-export function FillIn({ data, handlePersonalInfo }) {
+const FillIn = ({ data, handlePersonalInfo }) => {
   return (
     <div className="min-w-full sm:min-w-[425px] flex flex-col gap-4">
       <FillInPersonalInfo data={data} handlePersonalInfo={handlePersonalInfo} />
@@ -95,4 +115,6 @@ export function FillIn({ data, handlePersonalInfo }) {
       <FillInExperience data={data} />
     </div>
   );
-}
+};
+
+export { FillIn };
